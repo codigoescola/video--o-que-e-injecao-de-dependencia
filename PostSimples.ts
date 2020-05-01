9b1bcb4f812bf39ea7a6f22ac4f9ca895626ce7a
codigoescola <64652157+codigoescola@users.noreply.github.com>
2020-05-01 12:55:45 -0300

    Create PostSimples.ts

---
+++ PostSimples.ts
@@ -0,0 +1,26 @@
+class Notificador {
+    constructor(){}
+    notificarAgora() {
+        console.log('-> Notificando usuarios!');
+    }
+}
+
+console.log();
+
+'-> Novo Post: ' + titulo
+
+constructor(public titulo: string) {
+    console.log('-> Novo Post: ' + titulo);
+}
+
+publicar() {}
+publicar() {
+    console.log('-> Publicando Post!');
+    
+    let notificador = new Notificador();
+    notificador.notificarAgora();
+}
+
+new Post();
+let novoPost = new Post('Aprendendo Injecao de Dep!');
+novoPost.publicar();
